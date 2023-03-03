e8115464402340e87630e6f95711ce8cdeda79b7
Grekzxc <cool.grek456852@yandex.ru>
2023-03-03 13:04:49 +0300

    main

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Header  from './Header';
-import Technologies from './Technologies';
+import Header from './Components/Header';
+import NavBar from './Components/NavBar';
+import Profile from './Components/Profile';
 
 
 
@@ -10,17 +11,18 @@ import Technologies from './Technologies';
 let App = () => {
   return (
     <BrowserRouter>
-    <div>
-      <Header />
-      <Technologies/>
-      app hello
-      {/* <Route path={'/hello'} render = { ()=> <HelloMessage message={'hello friends'} /> } />    // 1 урок по типизауции
-      <Route path={'/bye'} render = { () => <ByeMessage message= {'bye friends'} /> }/> */}
-    </div>
+      <div className='app_Wrapper'>
+       <Header/>
+      <NavBar/>
+       <Profile/>
+      </div>
     </BrowserRouter>
   );
 }
 
+
+{/* <Route path={'/hello'} render = { ()=> <HelloMessage message={'hello friends'} /> } />    // 1 урок по типизауции
+      <Route path={'/bye'} render = { () => <ByeMessage message= {'bye friends'} /> }/> */}
 
 // type MessageType = {
 //   message: string
