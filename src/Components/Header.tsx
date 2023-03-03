51404f78e045a4fb1260de2039ce87880f845db6
Grekzxc <cool.grek456852@yandex.ru>
2023-03-03 14:56:40 +0300

    main

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import classes from './Header.module.css'
 
 function Header(){
     return(
-        <header className='header'>
+        <header className={classes.header}>
         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/500px-LEGO_logo.svg.png" alt="" />
       </header>
     )
--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
+import s from './Profile.module.css'
 
 function Profile(){
     return(
-        <div className='content'>
+        <div className={s.content}>
         <div>
           <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg' alt="" />
         </div>
         <div>ava+description</div>
         <div>
           My post
-          <div>
+          <div className={s.posts}>
             New post
           </div>
-          <div>post1</div>
-          <div>post2</div>
-          <div>post3</div>
+          <div className={s.item}>post1</div>
+          <div className={s.item}>post2</div>
+          <div className={s.item}>post3</div>
         </div>
 
       </div>
