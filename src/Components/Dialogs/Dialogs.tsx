894a032a1403d55dc1897aa75e5c871a8c512c0c
Grekzxc <cool.grek456852@yandex.ru>
2023-03-09 15:14:00 +0300

    21lesson

--- src/Components/Dialogs/Dialogs.tsx
+++ src/Components/Dialogs/Dialogs.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import s from './Dialogs.module.css'
 
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.item + ' ' + s.active}>
+                    Dimych
+                </div>
+                <div className={s.item}>
+                    Andrew
+                </div>
+                <div className={s.item}>
+                    Alex
+                </div>
+                <div className={s.item}>
+                    Kisa
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi, how are you</div>
+                <div className={s.message}>Hi, i am like you</div>
+                <div className={s.message}>Hi, your fine</div>
+            </div>
         </div>
+
     )
 }
 
